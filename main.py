21480ea904525af4e8ecaeb71d7a0809ab378cd5
3ngineer <aiman.mahrat@ryerson.ca>
2022-07-14 01:32:41 -0400

    Added class attribute, constructor, typings, initialized arguments and used magic attribute to display attributes

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 class item:
-    def calculate_total_price(self, x, y):
-        return x*y
-item1 = item()
-item1.name = "Phoone"
-item1.price = 100;
-item1.quantity=5;
-print(item1.calculate_total_price(item1.price, item1.quantity))
+    pay_rate =0.8 #Class attribute :The pay rate after 20% discount
+    def __init__(self, name:str, price:float, quantity=0):
+        # typing of each argument is specified and the quantity is initialized to 0 in the constructor
+        
+        # run validations to the recieved arguments
+        
+        assert price >=0, f"Price {price} is not grater than zero!"
+        assert quantity >=0, f"Quantity {quantity} is not grater than zero!"
+        
+        # Assign to self object
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+    def calculate_total_price(self):
+        return self.price * self.quantity
+    
+item1 = item("phone", 100, 1)
+item2 = item("laptop", 1000, 3)
 
-item2 = item()
-item2.name = "laptop"
-item2.price = 1000;
-item2.quantity=4;
-print(item2.calculate_total_price(item2.price, item2.quantity))
+print (item.__dict__) #Prints all attributes in class level
+print (item1.__dict__) #Prints all attributes in instance level
+
+
+
 
 
 
